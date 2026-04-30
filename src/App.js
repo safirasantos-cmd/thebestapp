381b4b4dbe91b9f69047bb0511f051d31509b6e2
SAFIRA KALINE CONCEIÇÃO SANTOS <safira_santos@estudante.sesisenai.org.br>
2026-04-30 15:01:42 -0300

    Atualização do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() {   
+const [pokemons, setPokemons] = useState({});
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(false);
+
+useEffect (() => {
+  const getData = async () => {
+    try{
+      const res = await
+     axios.get('https://pokeapi.co/api/v2/pokemon/eevee');
+      setPokemons(res.data);
+      console.log('Sucesso: ', res.data);
+      setLoading(false);
+      
+    }
+    catch (err) {
+      console.error(" Erro ao carregar API", err);
+      setLoading(false)
+      setError(true)
+    }
+  };
+  getData(); 
+}, {});
+
+
+   if (loading) return
+   <div
+   className="loader"> Carregando Pokemonlex</div>
+
+   if (error) return <div 
+   className="error"> Ocorreu um erro inesperado</div>;
+
+return(
+  <div className="App">
+   <h1> Poke Card</h1> 
+     <div className="pokemon-container">
+        <div className="pokemon-card">
+          <h3>{pokemons.name}</h3>
+          <img src={pokemons.sprites.front_shiny} alt={pokemons.name} />
+          <h3>{pokemons.stats[0].stat.name + ' ' + 
+pokemons.stats[0].base_stat + pokemons.stats[1].stat.name + ' ' +
+pokemons.stats[0].base_stat }</h3>
+             </div>
+         </div>
     </div>
   );
+ 
 }
 
 export default App;
